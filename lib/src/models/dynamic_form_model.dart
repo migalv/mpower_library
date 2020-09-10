e2210ad66a09240523fe0528942f6cd994942b19
Migalv <miguel@mpower.africa>
2020-09-10 11:48:01 +0200

    Added the "fromJson" to the DynamicForm model

--- lib/src/models/dynamic_form_model.dart
+++ lib/src/models/dynamic_form_model.dart
@@ -1,4 +1,6 @@
+import 'package:cons_calc_lib/cons_calc_lib.dart';
 import 'package:cons_calc_lib/src/models/question_model.dart';
+import 'package:meta/meta.dart';
 
 class DynamicForm {
   final String id;
@@ -18,6 +20,15 @@ class DynamicForm {
   }) {
     this.questions.sort((q1, q2) => q1.index.compareTo(q2.index));
   }
+
+  DynamicForm.fromJson(
+      {@required this.id, @required Map<String, dynamic> json, this.questions})
+      : title = json["title"],
+        emailList = json["email_list"] != null && json["email_list"].isNotEmpty
+            ? List<String>.from(json["email_list"])
+            : null,
+        greetingTitle = json["greeting_title"],
+        greetingSubtitle = json["greeting_subtitle"];
 
   @override
   String toString() {
